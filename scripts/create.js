909a6029cb006243d1f7610c9b259be20edc6ade
crtr0 <carter.rabasa@gmail.com>
2024-03-10 19:29:27 -0700

    added support for $vectorize

---
+++ scripts/create.js
@@ -0,0 +1,60 @@
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const {
+    ASTRA_DB_ENDPOINT,
+    ASTRA_DB_APPLICATION_TOKEN,
+} = process.env
+
+async function create() {
+    console.log("Creating the Vectorize collection")
+    let response = await fetch(`${ ASTRA_DB_ENDPOINT }/api/json/v1/default_keyspace`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'token': ASTRA_DB_APPLICATION_TOKEN
+        },
+        body: JSON.stringify({
+            createCollection: { 
+                name: "vectorize",
+                options: { 
+                    vector: {
+                        size: 1024 
+                    }, 
+                    vectorize: { 
+                        service: "nvidia", 
+                        options: { 
+                            modelName: "NV-Embed-QA" 
+                        } 
+                    } 
+                } 
+            } 
+        })
+    })
+    let json = await response.json()
+    console.log(json)
+    console.log("Creating the OpenAI collection")
+    response = await fetch(`${ ASTRA_DB_ENDPOINT }/api/json/v1/default_keyspace`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'token': ASTRA_DB_APPLICATION_TOKEN
+        },
+        body: JSON.stringify({
+            createCollection: { 
+                name: "openai",
+                options: { 
+                    vector: {
+                        size: 1536 
+                    } 
+                } 
+            } 
+        })
+    })
+    json = await response.json()
+    console.log(json)
+}
+
+create()
+
